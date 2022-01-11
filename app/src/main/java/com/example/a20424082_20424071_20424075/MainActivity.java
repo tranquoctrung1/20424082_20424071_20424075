4dce0583bfaca944b5715d312bdd632fe0fd792e
tranquoctrung1 <tranquoctrung.12k9@gmail.com>
2022-01-11 11:06:23 +0700

    init source

---
+++ app/src/main/java/com/example/a20424082_20424071_20424075/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a20424082_20424071_20424075;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
